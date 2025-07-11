4ae172a585ceb342cb5e540d2d5c8fa6eff4e594
Wendy Asmatico <wendyasmatico@MacBook-Air-de-Wendy.local>
2025-07-11 16:13:42 +0200

    operateur

--- operateur.php
+++ operateur.php
@@ -9,30 +9,30 @@ $y = false;
 
 // Exercice 1 
 
-echo "La somme de a et b est de " . $a += $b . "\n";
-echo "La différence de a et b est de " . $a -= $b . "\n";
-echo "Le produit de a et b est de " . $a *= $b . "\n";
-echo "Le quotient de a et b est de " . $a /= $b."\n";
-echo "Le reste de la division de a par b est de " . $a %= $b . "\n";
+echo "La somme de a et b est de " . ($a += $b) . "<br>";
+echo "La différence de a et b est de " . ($a -= $b). "<br>";
+echo "Le produit de a et b est de " . ($a *= $b) . "<br>";
+echo "Le quotient de a et b est de " . ($a /= $b). "<br>";
+echo "Le reste de la division de a par b est de " . ($a %= $b) .  "<br>";
 
 // Exercice 2
 
-echo "a == b donne " . ($a == $b ? 'true' : 'false') . "\n";
-echo "a != b donne " . ($a != $b ? 'true' : "false") . "\n";
-echo "a < b donne " . ($a < $b ? 'true' : 'false') . "\n";
-echo "a > b donne " . ($a > $b ? 'true' : 'false') . "\n";
-echo "a <= b donne " . ($a <= $b ? 'true' : 'false') . "\n";
-echo "a >= b donne " . ($a >= $b ? 'true' : 'false') . "\n";
+echo "a == b donne " . ($a == $b ? 'true' : 'false') . "<br>";
+echo "a != b donne " . ($a != $b ? 'true' : "false") . "<br>";
+echo "a < b donne " . ($a < $b ? 'true' : 'false') . "<br>" ;
+echo "a > b donne " . ($a > $b ? 'true' : 'false') . "<br>" ;
+echo "a <= b donne " . ($a <= $b ? 'true' : 'false') . "<br>";
+echo "a >= b donne " . ($a >= $b ? 'true' : 'false') . "<br>";
 
 // Exercice 3
 
-echo "x AND y donne " . (($x && $y) ? 'true' : 'false') . "\n";
-echo "x OR y donne " . (($x || $y) ? 'true' : 'false') . "\n";
-echo "x XOR y donne " . (($x xor $y) ? 'true' : "false") . "\n";
+echo "x AND y donne " . (($x && $y) ? 'true' : 'false') . "<br>";
+echo "x OR y donne " . (($x || $y) ? 'true' : 'false') . "<br>" ;
+echo "x XOR y donne " . (($x xor $y) ? 'true' : "false") . "<br>";
 
 // Exercice 4
 
-echo "Si j'ajoute A à b, j'obtiens " . $a + $b . "\n";
-echo "Si je multiplie b par c, j'obtiens " . $b * $c . "\n";
-echo "Si je soustrais c de b, j'obtiens " . $b - $c . "\n";
-echo "Le reste de b divisé par c est " . $b % $c . "\n";
+echo "Si j'ajoute A à b, j'obtiens " . ($a + $b) ."<br>";
+echo "Si je multiplie b par c, j'obtiens " . ($b * $c) . "<br>";
+echo "Si je soustrais c de b, j'obtiens " . ($b - $c) ."<br>";
+echo "Le reste de b divisé par c est " . ($c % $b) . "<br>";
